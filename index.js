158613a95657d6316c1ef4ff6c09c18bfc33b5f3
Daman Bual <damanbual@gmail.com>
2024-06-18 15:02:43 -0700

    played around with activity a lot and got stuck so used answer key at the end to help

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ fetchMyIP((error, ip) => {
     console.log("It didn't work!" , error);
     return;
   }
-  
+
   console.log('It worked! Returned IP:' , ip);
 });
--- iss.js
+++ iss.js
@@ -1,20 +1,24 @@
-
+// iss.js
 const needle = require('needle');
 
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
 const fetchMyIP = function(callback) {
-  needle.get('https://api.ipify.org?format=json', (error, response) => {
-    if (error) {
-      callback(error, null);
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
 
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${response.body}`;
-      callback(Error(msg), null);
-      return;
-    }
-    
-    const ip = response.body.ip;
+    const ip = body.ip;
     callback(null, ip);
   });
 };
